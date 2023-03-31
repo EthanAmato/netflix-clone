a6446bb2ce67335c86509d3553f4bd9b930fe3f4
EthanAmato <ethannamato@gmail.com>
2023-03-31 11:23:00 -0400

    added dynamic routing to movies based on ID and watch component that allows users to play movies

--- components/movieCard.tsx
+++ components/movieCard.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteButton from "./favoriteButton";
-
+import { useRouter } from "next/router";
 
 interface MovieCardProps {
     data: Record<string, any>;
@@ -12,6 +12,7 @@ interface MovieCardProps {
 
 
 const MovieCard = ({ data }: MovieCardProps) => {
+    const router = useRouter();
     return (
         <div className="group bg-zinc-900 col-span relative h-[12vw]">
             <img
@@ -63,7 +64,9 @@ const MovieCard = ({ data }: MovieCardProps) => {
                     alt="Thumbnail" />
                 <div className="absolute z-10 w-full p-2 transition shadow-md bg-zinc-800 lg:p-4 rounded-b-md">
                     <div className="flex flex-row items-center gap-3">
-                        <div className="flex items-center justify-center w-6 h-6 transition bg-white rounded-full cursor-pointer lg:w-10 lg:h-10 hover:bg-neutral-300">
+                        <div
+                            onClick={()=>router.push(`/watch/${data?.id}`)} 
+                            className="flex items-center justify-center w-6 h-6 transition bg-white rounded-full cursor-pointer lg:w-10 lg:h-10 hover:bg-neutral-300">
                             <BsFillPlayFill size={30} />
                         </div>
                         <FavoriteButton movieId={data?.id}/>
